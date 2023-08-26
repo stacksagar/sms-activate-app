58b9af1871e938df167e2037e068e78d55373f43
Sagar Roy <stacksagar@gmail.com>
2023-08-26 23:44:56 +0600

    solved redirect issue

--- src/app/auth/signin/SigninForm.tsx
+++ src/app/auth/signin/SigninForm.tsx
@@ -34,7 +34,6 @@ export default function SigninForm() {
         const data = await signIn("credentials", {
           email: values.email,
           password: values.password,
-          redirect: false,
         });
 
         if (data?.error) {
