ffd74dbec6c158b3e1d8d87dba046e467ec6de4c
Sagar Roy <stacksagar@gmail.com>
2023-09-06 11:47:52 +0600

    add cryptomus payment callback

--- src/pages/api/cryptomus/callback.ts
+++ src/pages/api/cryptomus/callback.ts
@@ -1,4 +1,5 @@
 import Res from "@/lib/server/Res";
+import Setting from "@/models/mongodb/Setting";
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(
@@ -7,6 +8,7 @@ export default async function handler(
 ) {
   try {
     console.log("/callback is work: ", req.body);
+    Setting.create({ public: { ...req.body } });
     res.status(200).json({ message: "ok" });
   } catch (error) {
     return Res.err(res, error);
