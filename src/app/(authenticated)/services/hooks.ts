e6f48daea10c9ca9acd3b7ace61a5d28c7d1cd89
Sagar Roy <stacksagar@gmail.com>
2023-09-12 00:16:50 +0600

    fixed problems

--- src/app/(authenticated)/services/hooks.ts
+++ src/app/(authenticated)/services/hooks.ts
@@ -90,7 +90,7 @@ export function useOrderNumber() {
 
         {
           start: "Creating your order...",
-          error: "Please try later",
+          error: "No numbers available!",
         }
       );
 
